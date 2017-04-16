0cc932f19f7a54da91ca770f7e63bb91e820e326
Brecht Savelkoul <brecht.savelkoul@gmail.com>
2017-04-16 23:13:30 +0200

    Padding for the people

--- layouts/pamphlet.js
+++ layouts/pamphlet.js
@@ -4,7 +4,7 @@ var render = require('quill-render')
 module.exports = function (state, emit) {
   return html`
     <article style="background-image:url('${state.entry.cover}')">
-      <div class="container">
+      <div class="surface">
         <header>
           Distilled Pamphlets
           <small>Issue ${state.entry.issue}, Volume ${state.entry.volume}</small>
