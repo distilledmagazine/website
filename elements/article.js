04af7d1531c1ae77d225cd87d8052b95e46965d5
Brecht Savelkoul <brecht.savelkoul@gmail.com>
2017-07-25 20:17:53 +0200

    Generate old magazine pages with pamphlets layout

---
+++ elements/article.js
@@ -0,0 +1,40 @@
+var html = require('choo/html')
+
+module.exports = function (article, emit) {
+  return html`
+    <article style="background-image:url('${article.cover || '/fallback.jpg'}')">
+      <div class="surface">
+        <header>
+          Distilled Pamphlets
+          <small>${article.publication}</small>
+        </header>
+        <hr>
+        <section class="info">
+          <h1>${article.title}</h1>
+          ${article.authors.sort().map(author)}
+        </section>
+        ${content(article)}
+      </div>
+    </article>
+  `
+}
+
+function author (name) {
+  return html`
+    <div class="author">${name}</div>
+  `
+}
+
+function content (article) {
+  var section = html`<section class="content"></section>`
+  section.innerHTML = article.date ? article.content + date(article.date) : article.content
+
+  return section
+}
+
+function date (input) {
+  var date = new Date(input)
+  var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+  return `<time>${months[date.getMonth()]} ${date.getYear() + 1900}</time>`
+}
---
+++ gulpfile.js
@@ -0,0 +1,108 @@
+var target = 'www'
+
+var autoprefixer = require('autoprefixer')
+var cssnano = require('cssnano')
+var folders = require('gulp-folders')
+var gulp = require('gulp')
+var marked = require('marked')
+var path = require('path')
+var postcss = require('gulp-postcss')
+var through = require('through2')
+var Vinyl = require('vinyl')
+var yml = require('js-yaml')
+
+var article = require('./elements/article.js')
+var head = require('./elements/head')
+
+gulp.task('default', ['assets', 'magazine:articles', 'styles'])
+
+gulp.task('magazine:articles', function () {
+    return gulp.src('magazine/**/*.md')
+        .pipe(press())
+        .pipe(gulp.dest(target))
+    
+})
+
+gulp.task('magazine:issues', folders('magazine', function (folder) {
+    var meta = {
+        url: 'https://distilled.pm/' + folder
+    }
+
+    return gulp.src(path.join('magazine', folder, '**/*.md'))
+        .pipe(press({concat: true, path: folder + '/index.html', meta: meta}))
+        .pipe(gulp.dest(target))
+})) 
+
+gulp.task('styles', function() {
+  var plugins = [
+    autoprefixer(),
+    cssnano()
+  ]
+
+  return gulp.src('assets/style.css')
+    .pipe(postcss(plugins))
+    .pipe(gulp.dest(target))
+})
+
+gulp.task('assets', function() {
+  return gulp.src(['assets/**', '!**/*.css']).pipe(gulp.dest(target))
+})
+
+/**
+ * Plugins:
+ */
+function press (opts) {
+    if (!opts) {
+        opts = {}
+    }
+    var concat = ''
+
+    return through.obj(function (post, encoding, cb) {
+        var name = path.basename(post.path, '.md').split('-').slice(3).join('-')
+        var parsed = jekyll(post.contents.toString(), name)
+        var content = article(parsed).toString()
+
+        if (opts.concat) {
+            concat += content
+            return cb()
+        }
+        cb(null, new Vinyl({
+            path: name + '/index.html',
+            contents: new Buffer(doc(parsed, content))
+        }))
+    }, function (cb) {
+        if (concat === '') {
+            return cb()
+        }
+
+        cb(null, new Vinyl({
+            path: opts.path || 'index.html',
+            contents: new Buffer(doc(opts.meta || {url: 'https://distilled.pm'}, concat))
+        }))
+    })
+}
+
+/**
+ * Helpers:
+ */
+function doc (parsed, content) {
+    return '<!doctype html><html>' + head(parsed) + '<body>' + content + '</body></html>'
+}
+
+function jekyll (txt, slug) {
+    var doc, content
+
+    if (/^---\n/.test(txt)) {
+        var parts = txt.split('\n---\n')
+        doc = yml.safeLoad(parts[0].replace(/^---\n/, ''))
+        content = parts.slice(1).join('\n---\n').replace(/^\n/, '')
+    }
+    else {
+        doc = {}
+        content = txt
+    }
+    doc.content = marked(content)
+    doc.url = 'https://distilled.pm/' + slug
+    
+    return doc
+}
